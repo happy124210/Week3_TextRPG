37213c1ff0d5fdd4e37682c2de1a46b4327a674b
고윤아 <0205ria@gmail.com>
2025-04-21 15:07:54 +0900

    ✨ Add: 기본 초기화 파일 및 출력 보조 파일 추가
    - 프로젝트 진입점 Program.cs 생성
    - 한글 인코딩 설정
    - 출력 스타일 통일을 위한 Utils.cs 파일 추가
    
    - 초기 버전
    - 지피티를 통해서 구현
    - 최대한 알아보기 쉽게 주석을 가득 넣어놓음

---
+++ TextRPG_Team25/Core/GameManager.cs
@@ -0,0 +1,28 @@
+﻿namespace TextRPG_Team25.Core
+{
+    internal class GameManager
+    {
+        // 싱글톤
+        private static GameManager instance;
+        public static GameManager Instance => instance ??= new GameManager();
+
+        // 초기화
+        public void Initialize() 
+        {  
+            
+        }
+
+        // 게임 실행
+        public void Run() 
+        {
+            
+        }
+
+        // 메인 메뉴
+        public void ShowMainMenu() 
+        {
+            
+        }
+
+    }
+}
---
+++ TextRPG_Team25/Core/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TextRPG_Team25.Core
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            GameManager.Instance.Initialize();
+            GameManager.Instance.Run();
+        }
+    }
+}
