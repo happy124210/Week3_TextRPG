2e36622edb59e1e5576795030fdf8a82b894d9f6
고윤아 <0205ria@gmail.com>
2025-04-21 19:04:54 +0900

    ✨ Add: PlayerAttack 메서드 추가
    - 플레이어 턴 공격 대미지 계산 함수
    - 추후 StartBattle 메서드 확인 후 추가 예정

--- TextRPG_Team25/Battle.cs
+++ TextRPG_Team25/Battle.cs
@@ -1,7 +1,19 @@
 ﻿namespace TextRPG_Team25
 {
-    internal class Battle
+    internal class Battle(Player player)
     {
+        private void PlayerAttack(Monster selected)
+        {
+            int baseAttack = player.attack;
+            int offset = (int)Math.Ceiling(baseAttack * 0.1f);
+            int damage = new Random().Next(baseAttack - offset, baseAttack + offset + 1);
 
+            selected.hp -= damage;
+
+            if (selected.hp <= 0)
+            {
+                // 텍스트 회색으로 처리
+            }
+        }
     }
 }
