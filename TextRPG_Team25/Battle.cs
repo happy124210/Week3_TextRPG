f76de28f0d7720e749dcbcbe5803cb86e53ff38e
garnet-sunshine <sunjnghy@naver.com>
2025-04-21 18:08:28 +0900

    ✨ Add: 전투종료 로직 추가
    - 전투 시작 전 previousHP 호출해줘야 합니다. (int previousHP = player.hp;)
    - 이전 체력 필요없으면 패배 시 (100 -> 0)으로 체력 변동되도록 하겠습니다.

---
+++ TextRPG_Team25/Battle.cs
@@ -0,0 +1,36 @@
+﻿using TextRPG_Team25.Core;
+
+namespace TextRPG_Team25
+{
+    internal class Battle
+    {
+        public static void BattleResult(List<Monster> monsters, Player player, int previousHP)
+        {
+            bool allDead = true;
+
+            foreach (Monster m in monsters)
+            {
+                if (m.hp > 0)
+                {
+                    allDead = false;
+                    break;
+                }
+            }
+
+            if (allDead)
+            {
+                Console.WriteLine("Victory!");
+                Console.WriteLine($"던전에서 몬스터 {monsters.Count}마리를 잡았습니다.");
+                return;
+            }
+
+            if (player.hp <= 0)
+            {
+                Console.WriteLine("You Lose...");
+                Console.WriteLine($"Lv.{player.level} {player.name}");
+                Console.WriteLine($"{previousHP} → 0");
+                return;
+            }
+        }
+    }
+}
