89db62cf4f57a14b2e82fd70ac22599b76b9ed7f
skyrunner8797 <tkdgns8797@gmail.com>
2025-04-21 17:37:10 +0900

    Add: 메인메뉴 클래스 생성
    - 게임 시작과 초기 진입을 위한 GameManager 클래스 생성
    - 원하는 항목으로의 이동을 처리

--- TextRPG_Team25/Core/GameManager.cs
+++ TextRPG_Team25/Core/GameManager.cs
@@ -1,4 +1,6 @@
-﻿namespace TextRPG_Team25.Core
+﻿using System;
+
+namespace TextRPG_Team25.Core
 {
     internal class GameManager
     {
@@ -15,13 +17,50 @@
         // 게임 실행
         public void Run() 
         {
-            
+            Initialize();
+            ShowMainMenu();
         }
 
         // 메인 메뉴
         public void ShowMainMenu() 
         {
-            
+            Console.Clear();
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("이제 전투를 시작할 수 있습니다.\n");
+            Console.WriteLine("1. 상태 보기");
+            Console.WriteLine("2. 전투 시작\n");
+            Console.Write("원하시는 행동을 입력해주세요.\n>> ");
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    ShowStatus();
+                    break;
+                case "2":
+                    StartBattle();
+                    break;
+                default:
+                    Console.WriteLine("\n잘못된 입력입니다.");
+                    Console.ReadLine();
+                    ShowMainMenu();
+                    break;
+            }
+        }
+        
+        public void ShowStatus()
+        {
+            Console.Clear();
+            Console.ReadLine();
+            ShowMainMenu();
+        }
+
+        public void StartBattle()
+        {
+            Console.Clear();
+            Console.ReadLine();
+            ShowMainMenu();
         }
 
     }
