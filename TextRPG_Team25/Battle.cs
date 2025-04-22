2f1710e373a999ba2333d77810aa06f4239fb658
Hae1385 <zx3248@naver.com>
2025-04-22 10:38:31 +0900

    Update Battle.cs

--- TextRPG_Team25/Battle.cs
+++ TextRPG_Team25/Battle.cs
@@ -14,7 +14,7 @@ namespace TextRPG_Team25
         }
         public void MonsterAttackWindow()
         {
-            Console.WriteLine($"Lv.{monster.level} {monster.name} 의 공격!");
+            Console.WriteLine($"Lv.{FieldMonster[battleMonster].level} {FieldMonster[battleMonster].name} 의 공격!");
             Console.WriteLine($"Lv.{player.level} {player.name}을(를) 맞췄습니다.");
             Console.WriteLine();
             Console.WriteLine($"Lv.{player.level} {player.name}");
@@ -25,7 +25,7 @@ namespace TextRPG_Team25
             
             double monsterDmg;
 
-            for (int battleMonster = 0; battleMonster <= MonsterList.Count; battleMonster++)  //랜덤으로 생성된 적 개체수만큼 실행
+            for (int battleMonster = 0; battleMonster <= FieldMonster.Count; battleMonster++)  //랜덤으로 생성된 적 개체수만큼 실행
             {
                 int randomDmg = new Random().Next(1, 101);
 
@@ -35,7 +35,7 @@ namespace TextRPG_Team25
                     {
                         if (randomDmg <= 10) 
                         { 
-                        double i = monster.attack; i = i * 90 / 100; monsterDmg = Math.Round(i);  //10퍼센트 확률로 90퍼센트의 데미지 반올림
+                        double i = FieldMonster[battleMonster].attack; i = i * 90 / 100; monsterDmg = Math.Round(i);  //10퍼센트 확률로 90퍼센트의 데미지 반올림
                         MonsterAttackWindow();
                         Console.WriteLine(BattleHP -monsterDmg);
                         Console.WriteLine($"받은 피해 : {monsterDmg}");// 플레이어가 받은피해와 플레이어 체력 출력;
@@ -43,7 +43,7 @@ namespace TextRPG_Team25
                        }
                     else if (randomDmg >= 90) 
                         { 
-                        double i = monster.attack; i = i * 90 / 100; monsterDmg = Math.Round(i);  //10퍼센트의 확률로 110퍼센트의 데미지 반올림
+                        double i = FieldMonster[battleMonster].attack; i = i * 90 / 100; monsterDmg = Math.Round(i);  //10퍼센트의 확률로 110퍼센트의 데미지 반올림
                         MonsterAttackWindow();
                         Console.WriteLine(BattleHP - monsterDmg);
                         Console.WriteLine($"받은 피해 : {monsterDmg}");// 플레이어가 받은피해와 플레이어 체력 출력
@@ -51,14 +51,14 @@ namespace TextRPG_Team25
                     }
                     else
                         {
-                        double i = monster.attack;
+                        double i = FieldMonster[battleMonster].attack = monsterDmg;
                         MonsterAttackWindow();
                         Console.WriteLine(BattleHP - monsterDmg);
                         Console.WriteLine($"받은 피해 : {monsterDmg}");// 플레이어가 받은피해와 플레이어 체력 출력
                         BattleHP -= monsterDmg; //위의 랜덤공격력으로 플레이어 HP감소
                         }
                     }
-            if (BattleHP <= 0) { battleMonster = MonsterList.Count + 1; } //플레이어의 전투HP가 0이되면 for문에서 
+            if (BattleHP <= 0) { battleMonster = FieldMonster[battleMonster].Count + 1; } //플레이어의 전투HP가 0이되면 for문에서 
             }
         }
 
