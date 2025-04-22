483bc5449c894d1cf51ef0a5a1cdb298f39da791
Hae1385 <zx3248@naver.com>
2025-04-22 09:28:34 +0900

    Update Battle.cs

--- TextRPG_Team25/Battle.cs
+++ TextRPG_Team25/Battle.cs
@@ -1,42 +1,63 @@
-﻿using System.Numerics;
+﻿using System.ComponentModel.Design;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 
 namespace TextRPG_Team25
 {
     internal class Battle
     {
+        public double BattleHP { get; set; }
+        public void Battlehp(double battlehp)
+        {
+            battlehp = player.hp;
+        }
+        public void MonsterAttackWindow()
+        {
+            Console.WriteLine($"Lv.{monster.level} {monster.name} 의 공격!");
+            Console.WriteLine($"Lv.{player.level} {player.name}을(를) 맞췄습니다. [데미지 : {MonsterAttack.enermyDmg}]");
+            Console.WriteLine();
+            Console.WriteLine($"Lv.{player.level} {Player.name}");
+            Console.Write($"HP {battleHP} -> "); // 플레이어가 받은피해와 플레이어 체력 출력
+        }
         public void MonsterAttack()
         {
-            int battleHp = player.hp; //전투에서쓰는 HP따로 선언
+            
+            double monsterDmg;
 
-
-        for (int battleEnermy = 0; battleEnermy <= EnermyList.Count; battleEnermy++)  //랜덤으로 생성된 적 개체수만큼 실행
+            for (int battleMonster = 0; battleMonster <= MonsterList.Count; battleMonster++)  //랜덤으로 생성된 적 개체수만큼 실행
             {
+                int randomDmg = new Random().Next(1, 101);
 
-                int randomDmg = new Random().Next(1, 11) //적 공격력의 오차값 10퍼센트
+                if (MonsterList[i].MonsterDead == true) { randomDmg = 0; }
 
-
-        if (randomDmg == 1)
-                {
-                    if (randomDmg == 1) { double i = Monster.attack; i = i * 90 / 100; double enermyDmg = Math.Round(i); }     //10퍼센트 확률로 90퍼센트의 데미지 반올림
-                    else if (randomDmg == 10)
+                    else
                     {
-                        if (randomDmg == 1) { double i = Monster.attack; i = i * 90 / 100; double enermyDmg = Math.Round(i); } //10퍼센트의 확률로 110퍼센트의 데미지 반올림
-
-
-                        if (EnermyList[i].EnermyDead == true) { randomDmg = 0 }
-
-                        Console.WriteLine($"Lv.{Monster.level} {Monster.name} 의 공격!");
-                        Console.WriteLine($"Lv.{Player.level} {Player.name}을(를) 맞췄습니다. [데미지 : {enermyDmg}]");
-                        Console.WriteLine();
-                        Console.WriteLine($"Lv.{Player.level} {Player.name}")
-                        Console.WriteLine($"HP {battleHP} -> {battleHP - enermyDmg}") // 플레이어가 받은피해와 플레이어 체력 출력
-
-
-                    battleHP -= enermyDmg //위의 랜덤공격력으로 플레이어 HP감소
-
-                    if (battleHP <= 0) { battleEnermy = EnermyList.Count + 1 } //플레이어의 전투HP가 0이되면 for문에서 빠져나오기
+                        if (randomDmg <= 10) 
+                        { 
+                        double i = monster.attack; i = i * 90 / 100; monsterDmg = Math.Round(i);  //10퍼센트 확률로 90퍼센트의 데미지 반올림
+                        MonsterAttackWindow();
+                        Console.WriteLine(BattleHP -monsterDmg); // 플레이어가 받은피해와 플레이어 체력 출력;
+                        BattleHP -= monsterDmg; //위의 랜덤공격력으로 플레이어 HP감소
+                       }
+                    else if (randomDmg >= 90) 
+                        { 
+                        double i = monster.attack; i = i * 90 / 100; monsterDmg = Math.Round(i);  //10퍼센트의 확률로 110퍼센트의 데미지 반올림
+                        MonsterAttackWindow();
+                        Console.WriteLine(BattleHP - monsterDmg); // 플레이어가 받은피해와 플레이어 체력 출력
+                        BattleHP -= monsterDmg; //위의 랜덤공격력으로 플레이어 HP감소
                     }
-                }
+                    else
+                        {
+                        double i = monster.attack;
+                        MonsterAttackWindow();
+                        Console.WriteLine(BattleHP - monsterDmg); // 플레이어가 받은피해와 플레이어 체력 출력
+                        BattleHP -= monsterDmg; //위의 랜덤공격력으로 플레이어 HP감소
+                        }
+                    }
+            if (BattleHP <= 0) { battleMonster = MonsterList.Count + 1; } //플레이어의 전투HP가 0이되면 for문에서 
+            }
+        }
+
     }
 }
